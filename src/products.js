a2afe570ab4204ac5ad7423e029e27246e99a5da
Barry Deeney <mxaddict@codedmaster.com>
2018-12-16 22:03:18 +0800

    Added product loader

--- src/products.js
+++ src/products.js
@@ -1,9 +1,116 @@
+const axios = require('axios')
 const log = console.log
 
+// Timeout after 20 seconds
+axios.defaults.timeout = 1000*60
+
+// Tell the server we are using Chrome
+axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
+
+// Load error codes
+const errors = require('./errors')
+
 module.exports = function (db) {
-  db.query('SELECT COUNT(*) AS count FROM products', (error, results, fields) => {
-    if (error) throw error
-    log('PRODUCTS COUNT: ' + results[0].count)
-  })
-  db.end()
+  async function loadProducts(shop) {
+    let getMore = true
+    let page = 1
+    let limit = 250
+    let status = 4
+
+    process.stdout.write('+')
+
+    // Mark as processing
+    db.query('UPDATE shops SET status = 3 WHERE id = ' + shop.id)
+
+    // Keep loading the products pages
+    while (getMore) {
+      let url = shop.name + '/products.json?limit=' + limit + '&page=' + page
+      let products = []
+
+      try {
+        let res = await axios.get(url)
+
+        if (res.data) {
+          try {
+            if (res.data.products) {
+              products = res.data.products
+
+              for (let i = 0, len = products.length; i < len; i++) {
+                let product = products[i]
+                let variant = product.variants[0]
+                let image = product.images[0]
+
+                db.query(
+                  "INSERT INTO products SET " +
+                  "shop_id = " + db.escape(shop.id) + ", " +
+                  "title = " + db.escape(product.title) + ", " +
+                  "product_type = " + db.escape(product.product_type) + ", " +
+                  "link = " + db.escape(shop.name + "/products/" + product.handle + "?variant=" + variant.id) + ", " +
+                  "tags = " + db.escape(JSON.stringify(product.tags)) + ", " +
+                  "image = " + db.escape((image ? image.src : '')) + ", " +
+                  "price = " + db.escape(variant.price) + ", " +
+                  "raw_data = " + db.escape(JSON.stringify(product)) + ", " +
+                  "created_at = NOW(), " +
+                  "updated_at = NOW()"
+                )
+
+                process.stdout.write('+')
+              }
+            } else {
+              // Not a shopify page
+              process.stdout.write('-')
+              status = errors['NOTSHOPIFY']
+            }
+          } catch (e) {
+            log(res.status)
+            log(e)
+            log(url)
+            // Not a shopify page
+            process.stdout.write('-')
+            status = errors['NOTSHOPIFY']
+          }
+        } else {
+          // What the hell went wrong?
+          process.stdout.write('-')
+          status = errors['UNKNOWN']
+        }
+      } catch (e) {
+        if (page === 1) {
+          // Not a shopify page
+          process.stdout.write('-')
+          status = errors['NOTSHOPIFY']
+        }
+      }
+
+      if (products.length < limit) {
+        getMore = false
+      } else {
+        page++
+        process.stdout.write('P')
+      }
+    }
+
+    // Mark as done
+    process.stdout.write('|')
+    db.query('UPDATE shops SET status = ' + status + ' WHERE id = ' + shop.id)
+  }
+
+  function loadBatch() {
+    db.query('SELECT * FROM shops WHERE status = 2 ORDER BY RAND() LIMIT 1', async (error, results, fields) => {
+      if (error) throw error
+
+      if (results.length == 0) {
+        log('ALL DONE')
+        return
+      }
+
+      let shop = results[0]
+
+      await loadProducts(shop)
+
+      loadBatch()
+    })
+  }
+
+  loadBatch()
 }
--- src/shops.js
+++ src/shops.js
@@ -1,26 +1,17 @@
 const axios = require('axios')
 const log = console.log
 
+// Timeout after 20 seconds
 axios.defaults.timeout = 1000*20
+
+// Tell the server we are using Chrome
 axios.defaults.headers.common['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
 
-errors = {
-  'NOT200': -1,
-  'NORESPONSE': -2,
-  'ENOTFOUND': -3,
-  'ERR_TLS_CERT_ALTNAME_INVALID': -4,
-  'ECONNREFUSED': -5,
-  'CERT_HAS_EXPIRED': -6,
-  'ECONNRESET': -7,
-  'EHOSTUNREACH': -8,
-  'ECONNABORTED': -9,
-  'DEPTH_ZERO_SELF_SIGNED_CERT': -10,
-  'EPROTO': -11,
-  'UNABLE_TO_VERIFY_LEAF_SIGNATURE': -12,
-}
+// Load error codes
+const errors = require('./errors')
 
 module.exports = function (db) {
-  let batch = 256
+  let batch = 128
   let promises = []
 
   function loadBatch() {
@@ -35,10 +26,10 @@ module.exports = function (db) {
       for (let i = 0, len = results.length; i < len; i++) {
         let shop = results[i]
         let req = axios.head(shop.name).then(res => {
-            process.stdout.write('+')
-            db.query('UPDATE shops SET status = 2 WHERE id = ' + shop.id)
+          process.stdout.write('+')
+          db.query('UPDATE shops SET status = 2 WHERE id = ' + shop.id)
         }).catch(err => {
-          let status = -666
+          let status = errors['UNKNOWN']
           if (err.response) {
             status = errors['NOT200']
           } else if (err.request) {
